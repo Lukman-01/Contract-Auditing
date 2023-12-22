c393d7562af63c680d9cbace14239efe3f51d5d0
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-22 23:59:52 +0100

    test

--- test/Lock.js
+++ test/Lock.js
@@ -1,126 +1,65 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe('StorageVictimResolved', function () {
+let storageVictimResolved;
+let owner;
+let user1;
+let user2;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+beforeEach(async function () {
+const StorageVictimResolved = await ethers.getContractFactory('StorageVictimResolved');
+[owner, user1, user2] = await ethers.getSigners();
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
+storageVictimResolved = await StorageVictimResolved.deploy();
+await storageVictimResolved.deployed();
 });
+
+it('should store and retrieve user data correctly', async function () {
+const amount = 100;
+const amount2 = 200;
+
+// Connect with user1 signer
+const storageVictimResolvedUser1 = storageVictimResolved.connect(user1);
+
+// Call the store function to store user1's data
+await storageVictimResolvedUser1.store(amount);
+
+// Call the getStore function to retrieve the stored data
+const [storedUser1, storedAmount1] = await storageVictimResolvedUser1.getStore();
+
+console.log(`User1 - Stored User: ${storedUser1}`);
+console.log(`User1 - Stored Amount: ${storedAmount1}`);
+console.log("\n");
+
+// Assert that the retrieved data matches the stored data
+expect(storedUser1).to.equal(user1.address);
+expect(storedAmount1).to.equal(amount);
+
+// Connect with user2 signer
+const storageVictimResolvedUser2 = storageVictimResolved.connect(user2);
+
+// Call the store function to store user2's data
+await storageVictimResolvedUser2.store(amount2);
+
+// Call the getStore function to retrieve the stored data
+const [storedUser2, storedAmount2] = await storageVictimResolvedUser2.getStore();
+
+console.log(`User2 - Stored User: ${storedUser2}`);
+console.log(`User2 - Stored Amount: ${storedAmount2}`);
+
+// Assert that the retrieved data matches the stored data
+expect(storedUser2).to.equal(user2.address);
+expect(storedAmount2).to.equal(amount2);
+});
+
+it('should return the correct owner', async function () {
+const contractOwner = await storageVictimResolved.getOwner();
+
+console.log(`Contract Owner: ${contractOwner}`);
+
+// Assert that the owner is the same as the owner signer's address
+expect(contractOwner).to.equal(owner.address);
+});
+});
+
