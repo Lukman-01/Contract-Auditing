e205f34bc7463dd42531dd00adab858ecc75f0bf
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-22 23:58:31 +0100

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+solidity: "0.8.19",
+networks: {
+hardhat: {
+chainId: 1337,
+},
+},
+paths: {
+artifacts: "./artifacts",
+cache: "./cache",
+sources: "./contracts",
+tests: "./test",
+},
 };
